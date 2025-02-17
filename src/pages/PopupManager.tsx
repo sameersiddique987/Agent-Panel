132fea6a53c0c63d6579ca79361152f596553725
sameersiddique987 <sameersiddique301@gmail.com>
2025-02-17 21:59:50 +0500

    Updated

--- src/pages/PopupManager.tsx
+++ src/pages/PopupManager.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { uploadPopupImage, fetchPopupImage } from "../api";
 
 const PopupManager = () => {
